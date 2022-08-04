8480f2db083ec953a84b5c55fe6f0c3a332b8956
whizkhaleel <whizkhaleel@gmail.com>
2022-08-04 02:59:35 -0700

    React Project Started

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Gwandu 2016 Old Boys
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
